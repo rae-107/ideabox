f9c7bc4f4914dbee97b761bd8c781245f07ac903
Rae Gebhart <raegebhart@gmail.com>
2022-11-02 15:21:43 -0600

    feat: Add query selector, event listeners and our idea class

---
+++ main.js
@@ -0,0 +1,50 @@
+//main.js file that contains all DOM related JavaScript.
+
+//Next step would be to create instances of our Idea Class
+
+// Variables
+var ideas = []
+
+// JQuery Selectors
+
+// Main
+// inputs
+// buttons
+var showStarredIdeaButton = document.querySelector('#show-starred-button')
+
+// Form
+// inputs
+var titleInputValue = document.querySelector('#title-input').value
+var bodyInputValue = document.querySelector('#body-input').value
+var searchInputValue = document.querySelector('#search-input')
+// buttons
+var searchButton = document.querySelector('#search-button')
+var saveButton = document.querySelector('#save-button')
+
+// Cards
+// inputs
+var ideaTitle = document.querySelector('.idea-title')
+var ideaBody = document.querySelector('.idea-body')
+// buttons
+var starButton = document.querySelector('#star-button')
+var deleteButton = document.querySelector('#delete-button')
+
+// Add Event Listeners
+showStarredIdeaButton.addEventListener('click', function() {
+
+})
+saveButton.addEventListener('click', function() {
+
+})
+searchButton.addEventListener('click', function() {
+
+})
+starButton.addEventListener('click', function() {
+
+})
+deleteButton.addEventListener('click', function() {
+
+})
+
+// Functions and Event Handlers
+
